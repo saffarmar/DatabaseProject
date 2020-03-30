b82a5edaa11867c08e3d9d068a2387d79c161974
saffarmar <mariam.saffar1@upr.edu>
2020-03-29 22:32:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from flask import Flask, request, render_template, flash, redirect, url_for, session, logging
 from passlib.hash import sha256_crypt
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
@@ -6,8 +8,18 @@ from dao.userDAO import UserDao
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'resource'
+app.config['SECRET_KEY'] = 'ciic4060'
 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please log in.', 'danger')
+            return redirect(url_for('login'))
+
+    return wrap
 
 @app.route('/')
 def home():
@@ -26,6 +38,7 @@ def login():
 
         user = dao.getUserByUsername(username)
 
+
         if user:
             # Get stored hash
             password = user['password']
@@ -36,6 +49,7 @@ def login():
                 session['logged_in'] = True
                 session['username'] = username
                 session['uid'] = user['uid']
+                session['role_type'] = user['role_type']
 
                 flash('You are now logged in.', 'success')
                 return redirect(url_for('home'))
